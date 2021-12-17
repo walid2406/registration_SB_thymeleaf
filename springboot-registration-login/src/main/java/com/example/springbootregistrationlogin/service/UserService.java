6d3fb8ad5d3f6579d555b2c14590a66764534b7e
walid.abboud.tech <walid.abboud.tech@gmail.com>
2021-12-17 17:18:27 +0100

    part 5 security layer on bootstring starter dependency creating security Cinfigration class annotated as configuration and enabled web security
    extends web adapter
    
    creation des Beean
    
    s

--- springboot-registration-login/src/main/java/com/example/springbootregistrationlogin/service/UserService.java
+++ springboot-registration-login/src/main/java/com/example/springbootregistrationlogin/service/UserService.java
@@ -2,8 +2,9 @@ package com.example.springbootregistrationlogin.service;
 
 import com.example.springbootregistrationlogin.model.User;
 import com.example.springbootregistrationlogin.web.dto.UserRegistrationDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 
 
     User save(UserRegistrationDto registrationDto);
--- springboot-registration-login/src/main/java/com/example/springbootregistrationlogin/service/UserServiceImpl.java
+++ springboot-registration-login/src/main/java/com/example/springbootregistrationlogin/service/UserServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.springbootregistrationlogin.model.User;
 import com.example.springbootregistrationlogin.repositry.UserRepositry;
 import com.example.springbootregistrationlogin.web.dto.UserRegistrationDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Array;
@@ -30,5 +32,12 @@ public class UserServiceImpl implements UserService {
         User user= new User(registrationDto.getFirstname(),registrationDto.getLastName(),
                 registrationDto.getEmail(),registrationDto.getPassword(), Arrays.asList(new Role("ROLE_USER")));
         return userRepositry.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        //Todo auto-generated method stub
+        return null;
     }
 }
